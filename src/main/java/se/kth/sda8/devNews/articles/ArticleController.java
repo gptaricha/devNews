e5461359ae22185a040839ad7ad6620bc3012cd4
Richa Gupta <gptaricha@gmail.com>
2020-11-12 19:08:30 +0100

    implement REST API for articles

--- src/main/java/se/kth/sda8/devNews/articles/ArticleController.java
+++ src/main/java/se/kth/sda8/devNews/articles/ArticleController.java
@@ -1,25 +1,38 @@
 package se.kth.sda8.devNews.articles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-
-@RequestMapping("/")
+@RequestMapping("/articles")
 public class ArticleController {
-    private ArticleService articleService = new ArticleService();
+    @Autowired
+    private ArticleService articleService;
     //Get a list of all the articles
-    @GetMapping("articles")
+    @GetMapping("")
     public List<Article> getAll() {
         return articleService.getAll();
     }
 
     // Get articles by ID
-    @GetMapping("articles/{id}")
+    @GetMapping("/{id}")
     public Article getByID(@PathVariable Long id) {
         return articleService.getByID(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    //Create an article , RequestBody helps to pass the body
+    @PostMapping("")
+    public Article create(@RequestBody Article newArticle) {
+        return articleService.create(newArticle);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        articleService.delete(id);
+    }
+
 }
--- src/main/java/se/kth/sda8/devNews/articles/ArticleService.java
+++ src/main/java/se/kth/sda8/devNews/articles/ArticleService.java
@@ -1,15 +1,20 @@
 package se.kth.sda8.devNews.articles;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Service
 public class ArticleService {
+    private static Long idCounter = 1L;
     private static List<Article> articleList= new ArrayList<>();
     // ask Babe
     static {
-        articleList.add(new Article(1L, "laundry", "kite", "richa"));
-        articleList.add(new Article(2L, "kitchen", "cook today", "richa"));
+        articleList.add(new Article(3L, "laundry", "kite", "richa"));
+        articleList.add(new Article(5L, "kitchen", "cook today", "richa"));
     }
     public List<Article> getAll() {
         return articleList;
@@ -20,6 +25,23 @@ public class ArticleService {
         return articleList.stream()
                 .filter(article -> article.getId().equals(id))
                 .findFirst();
+    }
+    // Create a new task as entered by the user
+    public Article create(Article newArticle) {
+        add(newArticle);
+        return newArticle;
+    }
 
+    private static boolean add(Article newArticle) {
+        //generate and set the id
+        newArticle.setId(idCounter);
+        idCounter++;
+        return articleList.add(newArticle);
+    }
+
+    public void delete(Long id) {
+        articleList = articleList.stream()
+                                    .filter(t -> !t.getId().equals(id))
+                                    .collect(Collectors.toList());
     }
 }
